6641b0c31213a02b1d7c1b3c1b27ad53a2b8f95c
Pablo <pablo.ntn2020@gmail.com>
2020-10-19 20:50:51 -0300

    testando

--- src/controller/analysePixel.js
+++ src/controller/analysePixel.js
@@ -13,48 +13,52 @@ module.exports = {
                 res.json(err);
                 return;
             } else {
-                var R = pixels.data[0],
-                    G = pixels.data[1],
-                    B = pixels.data[2];
-
-                colorYellow = await connection('yellow')
-                    .whereBetween('R', [R - 20, R + 20])
-                    .whereBetween('G', [G - 50, G + 50])
-                    .whereBetween('B', [B - 100, B + 100]);
-                colorSum.push(colorYellow.length)
-
-                colorRed = await connection('red')
-                    .whereBetween('R', [R - 20, R + 20])
-                    .whereBetween('G', [G - 20, G + 20])
-                    .whereBetween('B', [B - 20, B + 20]);
-                colorSum.push(colorRed.length)
-
-                colorPurple = await connection('purple')
-                    .whereBetween('R', [R - 20, R + 20])
-                    .whereBetween('G', [G - 20, G + 20])
-                    .whereBetween('B', [B - 20, B + 20]);
-                colorSum.push(colorPurple.length)
-
-                colorOrange = await connection('orange')
-                    .whereBetween('R', [R - 20, R + 20])
-                    .whereBetween('G', [G - 20, G + 20])
-                    .whereBetween('B', [B - 20, B + 20]);
-                colorSum.push(colorOrange.length)
-
-                colorGreen = await connection('green')
-                    .whereBetween('R', [R - 20, R + 20])
-                    .whereBetween('G', [G - 20, G + 20])
-                    .whereBetween('B', [B - 20, B + 20]);
-                colorSum.push(colorGreen.length)
-
-                var maior = Math.max.apply(null, colorSum);
-                var casa = 0;
-                for (var i = 0; i <= colorSum.length - 1; i++) {
-                    if (colorSum[i] == maior)
-                        casa = i;
+                for (var i = 0; i <= 7; i += 4) {
+                    var R = pixels.data[i],
+                        G = pixels.data[i + 1],
+                        B = pixels.data[i + 2];
+                    teste(R, G, B)
                 }
-                console.log(colorsName[casa])
             }
         });
     }
+}
+async function teste(R, G, B) {
+    colorYellow = await connection('yellow')
+        .whereBetween('R', [R - 20, R + 20])
+        .whereBetween('G', [G - 50, G + 50])
+        .whereBetween('B', [B - 100, B + 100]);
+    colorSum.push(colorYellow.length)
+
+    colorRed = await connection('red')
+        .whereBetween('R', [R - 20, R + 20])
+        .whereBetween('G', [G - 20, G + 20])
+        .whereBetween('B', [B - 20, B + 20]);
+    colorSum.push(colorRed.length)
+
+    colorPurple = await connection('purple')
+        .whereBetween('R', [R - 20, R + 20])
+        .whereBetween('G', [G - 20, G + 20])
+        .whereBetween('B', [B - 20, B + 20]);
+    colorSum.push(colorPurple.length)
+
+    colorOrange = await connection('orange')
+        .whereBetween('R', [R - 20, R + 20])
+        .whereBetween('G', [G - 20, G + 20])
+        .whereBetween('B', [B - 20, B + 20]);
+    colorSum.push(colorOrange.length)
+
+    colorGreen = await connection('green')
+        .whereBetween('R', [R - 20, R + 20])
+        .whereBetween('G', [G - 20, G + 20])
+        .whereBetween('B', [B - 20, B + 20]);
+    colorSum.push(colorGreen.length)
+
+    var maior = Math.max.apply(null, colorSum);
+    var casa = 0;
+    for (var i = 0; i <= colorSum.length - 1; i++) {
+        if (colorSum[i] == maior)
+            casa = i;
+    }
+    console.log(colorsName[casa])
 }
